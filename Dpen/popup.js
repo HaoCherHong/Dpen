c4100343c5c472eb6f0b7570814eedcca811d504
Hao-Cher Hong <rax333j@gmail.com>
2015-05-14 14:17:09 +0800

    v 0.1

--- Dpen/popup.js
+++ Dpen/popup.js
@@ -1,6 +1,5 @@
 //----------------GA-----------------
-var debug_enabled = false;
-var _AnalyticsCode = '';
+var _AnalyticsCode = 'UA-61516445-3';
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', _AnalyticsCode]);
@@ -19,12 +18,9 @@ _gaq.push(['_trackPageview']);
 function sendInvitation(first) {
 	getAPI('dcard/accept?first=' + first, function(result, error) {
 		if(!error) {
-			if(!result.error) {
-				//success
-				window.close();
-			} else {
-				alert(result.msg)
-			}
+			_gaq.push(['_trackEvent', 'action', 'Send Invitation']);
+			//success
+			window.location.reload()
 		} else {
 			alert(error)
 		}
@@ -36,7 +32,12 @@ function getAPI(url, callback) {
 	xhr.onreadystatechange = function() {
 	  if (xhr.readyState == 4) {
 	  	if(xhr.status === 200) {
-	  		callback(JSON.parse(xhr.responseText))
+	  		var response = JSON.parse(xhr.responseText)
+	  		if(!response.error) {
+	  			callback(response)
+		  	} else {
+		  		callback(null, response.msg)
+		  	}
 	  	} else {
 	  		callback(null, 'Error ' + xhr.status)
 	  	}
@@ -65,6 +66,9 @@ function showDcard() {
 	show(document.getElementById('dcard'))
 	//Show dcard photo
 	document.getElementById('dcard_photo').src = 'https://www.dcard.tw/api/dcard/photo'
+	//Fake photo
+	// document.getElementById('dcard_photo').src = 'https://www.dcard.tw/img/scratchpad/ducks.jpg'
+	
 	//Fill data
 	getAPI('dcard/', function(result, error) {
 		if(!error) {
@@ -96,7 +100,7 @@ function showDcard() {
 			})
 
 			//Show invitation button or label
-			if(true || !result.connection.accept) {
+			if(!result.connection.accept) {
 				showFormButton.addEventListener('click', function() {
 					hide(document.getElementById('dcard_table'))
 					show(document.getElementById('invitation_form'))
@@ -110,6 +114,24 @@ function showDcard() {
 					show(document.getElementById('label_invitation_sent'))
 				}
 			}
+
+			//Fake data
+			// result.dcard = {
+	  //           gender: "F",
+	  //           school: "新天鵝堡大學",
+	  //           department: "治癒系",
+	  //           grade: "Lv.max",
+	  //           talent: "專長:脹氣、上電視 興趣: 仰望星空、載浮載沉",
+	  //           club: "沒有任何室內活動的社團願意接受我QAQ",
+	  //           lecture: "游泳課、激勵講座、治癒講座",
+	  //           lovedcountry: "亞洲地區都不錯",
+	  //           trouble: "亞洲地區都不錯，人比周邊商品還多",
+	  //           wanttotry: "最近比較洩氣，情緒不穩又常常爆氣。冬天風太大",
+	  //           exchange: "如何從小變大、如何激勵自己替自己打氣",
+	  //           photo: "./img/scratchpad/ducks.jpg",
+	  //           createdAt: "2014-01-07T03:10:49.000Z",
+	  //           updatedAt: "2014-01-07T03:10:49.000Z"
+	  //       }
 
 			//Fill Dcard table
 			var genderTd = document.getElementById('dcard_gender')
@@ -142,13 +164,56 @@ function showDcard() {
 	})
 }
 
+function showNotificationLinks(status) {
+	//Show Notifications div
+	show(document.getElementById("notification_links"))
+
+	//Update Mails
+	var mailLink = document.getElementById("link_unread_mail")
+	mailLink.addEventListener('click', function() {
+		_gaq.push(['_trackEvent', 'click', 'mails link']);
+		var win = window.open('https://www.dcard.tw/notification', '_blank');
+		win.focus;
+	})
+	if(status.newsUnread) {
+		mailLink.className = "notify"
+		mailLink.innerText = "你有 " + status.newsUnread + " 封未讀信件"
+	}
+
+	//Update Notifications
+	getAPI('member/new_notification', function(response, error) {
+		var unreadNotifications = response.notification.filter(function(item) {
+			return item.seenAt <= item.updatedAt
+			}).length
+		var notificationLink = document.getElementById("link_unread_notification")
+		notificationLink.addEventListener('click', function() {
+			_gaq.push(['_trackEvent', 'click', 'notifications link']);
+			var win = window.open('https://www.dcard.tw/', '_blank');
+			win.focus;
+		})
+
+		if(unreadNotifications) {
+		notificationLink.className = "notify"
+		notificationLink.innerText = "你有 " + unreadNotifications + " 個未讀通知"
+	}
+	})
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+	_gaq.push(['_trackEvent', 'action', 'popup']);
 	//links
-	document.getElementById('link_dcard').addEventListener('click', function() {
-		var win = window.open('https://www.dcard.tw/', '_blank');
+	document.getElementById('logo').addEventListener('click', function() {
+		_gaq.push(['_trackEvent', 'click', 'logo']);
+		var win = window.open('https://www.dcard.tw/dcard', '_blank');
+		win.focus;
+	})
+	document.getElementById('link_share').addEventListener('click', function() {
+		_gaq.push(['_trackEvent', 'click', 'share']);
+		var win = window.open('https://chrome.google.com/webstore/detail/dpen/aflhihlkalkmbjgjhejhloapaljaohih/', '_blank');
 		win.focus;
 	})
 	document.getElementById('link_wootrans').addEventListener('click', function() {
+		_gaq.push(['_trackEvent', 'click', 'wootrans']);
 		var win = window.open('https://chrome.google.com/webstore/detail/wootrans/jonegeahehknbgnifdfbnidfpfigpdcp/', '_blank');
 		win.focus;
 	})
@@ -157,12 +222,16 @@ document.addEventListener('DOMContentLoaded', function() {
 	getAPI('member/status', function(status, error) {
 		if(!error) {
 			if(status.isLogin) {
+				//Show Dcard
 				showDcard();
+				//Show Notifications
+				showNotificationLinks(status);
 			} else {
 				//Show login button
 				var loginButton = document.getElementById('login_button');
 				show(loginButton);
 				loginButton.addEventListener('click', function() {
+					_gaq.push(['_trackEvent', 'action', 'login']);
 					var win = window.open('https://www.dcard.tw/login', '_blank');
 					win.focus();
 				})
@@ -172,4 +241,13 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 	
+	//Check if just installed
+	chrome.storage.sync.get('installedVersion', function(items) {
+		var currentVersion = chrome.app.getDetails().version
+		var installedVersion = items.installedVersion
+		if(installedVersion == undefined || currentVersion > installedVersion) {
+			chrome.storage.sync.set({'installedVersion': currentVersion})
+			_gaq.push(['_trackEvent', 'other', 'installed', currentVersion.toString()]);
+		}
+	})
 });
